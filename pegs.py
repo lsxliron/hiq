2b08284e6259ab8dce8653df04f1d77084260e1e
Liron Shimrony <lsxliron@gmail.com>
2014-03-31 22:11:19 -0400

    added docstrings

--- pegs.py
+++ pegs.py
@@ -13,12 +13,15 @@ def main():
 
 
 def get_list_of_possible_moves(board):
+	"""
+	Returns a list of the valid moves which exist on a board.
+	It does not mean that each move in that list is valid in every state
+	"""
 	moves = list()
 	for i in range(0, board.length):
 		for j in range(0, board.width):
-			# if (i * board.length) + j == 32:
-				# pdb.set_trace()
-			#Horizonatl moves
+
+			#Horizontal moves
 			if (j<board.length-2):
 				if board.grid[i][j] != -1:
 					if board.grid[i][j+1] != -1:
@@ -32,11 +35,12 @@ def get_list_of_possible_moves(board):
 											  (board.length*i) + j+1,
 											  (board.length*i) + j))
 
-				#Vertical moves:
+			#Vertical moves:
 			if (i<board.width-2):	
 				if board.grid[i][j] != -1:
 					if board.grid[i+1][j] != -1:
 						if board.grid[i+2][j] != -1:
+							#Create 2 moves (top down and bottom up)
 							moves.append(move((i * board.length) + j,
 											  (i * board.length) + j + board.length,
 											  (i * board.length) + j + 2*board.length))
@@ -55,7 +59,18 @@ def get_list_of_possible_moves(board):
 
 
 class move(object):
+	"""
+	This class represents a move on a solitare board. 
+	A move consists of 3 consecutive squares (horizonatl or vertical)
+	"""
 	def __init__(self, s1, s2, s3):
+		"""
+		Initializer
+		Parameters:
+		s1- the first square to move from
+		s2- the second square (on the move)
+		s3- the goal square
+		"""
 		self.s1 = s1
 		self.s2 = s2
 		self.s3 = s3
@@ -66,7 +81,17 @@ class move(object):
 		return str("({s1}, {s2}, {s3})".format(s1=self.s1, s2=self.s2, s3=self.s3))
 
 class Board(object):
+	"""
+	This class represents a peg solitare board.
+	"""
+
 	def __init__(self, length, width):
+		"""
+		Initializer
+		Parameters:
+		length- the length of the board
+		width- the width of the board
+		"""
 		self.length = length
 		self.width = width
 		self.grid= list()
@@ -86,6 +111,9 @@ class Board(object):
 
 
 	def __str__(self):
+		"""
+		Format the output when printing a board to the display
+		"""
 		output = ""
 		d=dict()
 		d[-1] = "--"
@@ -97,6 +125,24 @@ class Board(object):
 
 			output += "\n"
 		return output
+
+	def __eq__(self, other):
+		"""
+		Checks boards equality
+		"""
+		if self.length != other.length or 
+		   self.width != other.width:
+		   return False
+
+		  #Check elements
+		  for i in range(0,self.length):
+		  	for j in range(0, self.width):
+		  		if (self.grid[i][j] != other.grid[i][j]):
+		  			return False
+
+		  	return True
+
+
 
 
 
